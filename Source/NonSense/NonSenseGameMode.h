36435527bff5e20ad1e766341a531ee2107a3dc1
Kevin Ke <ke_qinyang@outlook.com>
2023-04-10 22:41:26 -0400

    Initial SetUp

---
+++ Source/NonSense/NonSenseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NonSenseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANonSenseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANonSenseGameMode();
+};
+
+
+
